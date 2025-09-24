a13c14c2a8aab4e1caa1271ad422604ec0235d8f
Yehor <bizonheroes999@gmail.com>
2025-09-24 16:11:31 +0300

    A base class has been created in the models: blog, core, travelog, users

--- travelog_project/blog_app/models.py
+++ travelog_project/blog_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users_app.models import Profile
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=50, choices=[('Travel', 'подорож'), ('Adventure','пригоди')])
+
+
--- travelog_project/travelog_app/models.py
+++ travelog_project/travelog_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users_app.models import Profile
 
-# Create your models here.
+class Trip(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    posts = models.ManyToManyField('blog_app.Post', blank=True)
